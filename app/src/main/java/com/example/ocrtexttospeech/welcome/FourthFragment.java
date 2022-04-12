e67ad2c289cb43fe0fc7011b1532de3e5632db0c
Jasur <jasur.shukurov29@gmail.com>
2022-04-12 15:13:16 -0400

    Intro Page Updated

--- app/src/main/java/com/example/ocrtexttospeech/welcome/FourthFragment.java
+++ app/src/main/java/com/example/ocrtexttospeech/welcome/FourthFragment.java
@@ -1,20 +1,23 @@
 package com.example.ocrtexttospeech.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.ocrtexttospeech.FrontPage;
 import com.example.ocrtexttospeech.R;
 
 import java.util.Locale;
 
-public class ThirdFragment extends Fragment {
+public class FourthFragment extends Fragment {
 
     TextToSpeech tts;
 
@@ -26,10 +29,10 @@ public class ThirdFragment extends Fragment {
                 new TextToSpeech.OnInitListener() {
                     @Override
                     public void onInit(final int status) {
-                        if(status==TextToSpeech.SUCCESS) {
+                        if (status == TextToSpeech.SUCCESS) {
                             Log.d("TTS Intro Page", "Text to Speech Engine started successfully.");
                             tts.setLanguage(Locale.US);
-                        }else{
+                        } else {
                             Log.d("TTS Intro Page", "Error starting text to speech engine.");
                         }
                     }
@@ -45,9 +48,9 @@ public class ThirdFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        if(this.isVisible())
-            tts.speak(getString(R.string.ocr_intro_page_instructions), TextToSpeech.QUEUE_ADD, null, null);
-        else{
+        if (this.isVisible())
+            tts.speak(getString(R.string.currency_detection_intro_page_instructions), TextToSpeech.QUEUE_ADD, null, null);
+        else {
             tts.stop();
         }
     }
@@ -62,8 +65,7 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(
             LayoutInflater inflater,
             ViewGroup container,
-            Bundle savedInstanceState)
-    {
+            Bundle savedInstanceState) {
 
         View view = inflater
                 .inflate(
@@ -73,8 +75,8 @@ public class ThirdFragment extends Fragment {
         TextView tvTitle = view.findViewById(R.id.title_tv);
         TextView tvInstructions = view.findViewById(R.id.instructions_tv);
 
-        tvTitle.setText(R.string.ocr_intro_title);
-        tvInstructions.setText(R.string.ocr_intro_page_instructions);
+        tvTitle.setText(R.string.currency_detection_intro_title);
+        tvInstructions.setText(R.string.currency_detection_intro_page_instructions);
 
 
         return view;
